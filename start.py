9fd7d25d3def5d3819943dea966d8509999659fb
Funky7Monkey <nathanjamesschimmel@gmail.com>
2016-04-06 17:05:03 -0400

    more testing...

--- start.py
+++ start.py
@@ -1,10 +1,10 @@
 from time import sleep
 import subprocess as sub
-p = sub.Popen('sudo','python3.5','bot.py',stdout=sub.PIPE,stderr=sub.PIPE)
+p = sub.Popen(['sudo','python3.5','bot.py'],stdout=sub.PIPE,stderr=sub.PIPE)
 while True:
 	if p.poll == False:
 		print('TEST')
 	if p.poll == True:
 		print('TEST2')
-		p = sub.Popen('sudo','python3.5','bot.py',stdout=sub.PIPE,stderr=sub.PIPE)
+		p = sub.Popen(['sudo','python3.5','bot.py'],stdout=sub.PIPE,stderr=sub.PIPE)
 	sleep(1)
