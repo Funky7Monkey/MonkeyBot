e6959ac572dbf97500027b68cd33131e214a5cad
Funky7Monkey <nathanjamesschimmel@gmail.com>
2016-04-06 16:57:22 -0400

    testing

--- start.py
+++ start.py
@@ -1,12 +1,9 @@
 from time import sleep
-import os
-p = os.popen('sudo python3.5 bot.py',"r")
+import subprocess as sub
+p = sub.Popen('sudo','python3.5','bot.py',stdout=sub.PIPE,stderr=sub.PIPE)
 while True:
 	if p.poll == False:
 		print('TEST')
-		line = p.readline()
-		if not line: break
-		print(line)
 	if p.poll == True:
 		print('TEST2')
 		p = os.popen('sudo python3.5 bot.py',"r")
