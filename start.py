aa0a7db9848a6dfe9472c86564989c06a54f810f
Funky7Monkey <nathanjamesschimmel@gmail.com>
2016-04-06 16:43:25 -0400

    Commit

--- start.py
+++ start.py
@@ -2,8 +2,9 @@ from time import sleep
 import os
 p = False
 while True:
-	p = os.popen('sudo python3.5 bot.py',"r")
-	while 1:
+	while p == False:
+		p = os.popen('sudo python3.5 bot.py',"r")
+	while p != False:
 		line = p.readline()
 		if not line: break
 		print(line)
