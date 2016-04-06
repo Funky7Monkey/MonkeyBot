6e533330405c375d9dc2e52dcaed468eca331547
Funky7Monkey <nathanjamesschimmel@gmail.com>
2016-04-06 16:29:47 -0400

    Keepalive adjustment

--- start.py
+++ start.py
@@ -1,5 +1,7 @@
 import subprocess as sub
+from time import sleep
 p = False
 while True:
 	if p == False:
-		p = sub.Popen(['sudo python3.5 bot.py'],stdout=sub.PIPE,stderr=sub.PIPE)
+		p = sub.Popen(['sudo', 'python3.5', 'bot.py'],stdout=sub.PIPE,stderr=sub.PIPE)
+	sleep(60)
