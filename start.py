0d23d442f655590d271331baf40e9f666562d6a2
Funky7Monkey <nathanjamesschimmel@gmail.com>
2016-04-06 16:40:31 -0400

    Commit

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ p = False
 while True:
 	p = os.popen('sudo python3.5 bot.py',"r")
 	while 1:
-    	line = p.readline()
-    	if not line: break
-    	print(line)
-    sleep(10)
+		line = p.readline()
+		if not line: break
+		print(line)
+	sleep(10)
