2cd613fcb5b3e7f2b71eaa410fd29c2f4cbb6854
Funky7Monkey <nathanjamesschimmel@gmail.com>
2016-04-07 21:13:10 -0400

    Commit

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ from time import sleep
 import subprocess as sub
 update = ['sudo','git','pull']
 bot = ['sudo','python3.5','bot.py']
-up = sub.Popen(update,stdout=sub.PIPE,stderr=sub.PIPE)
+up = sub.Popen(update)
 up.wait()
 p = sub.Popen(bot,stdout=sub.PIPE,stderr=sub.PIPE,universal_newlines=True)
 while True:
