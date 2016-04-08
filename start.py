6f7777336a7ad763594587f5d503a09cf13dbcc5
Funky7Monkey <nathanjamesschimmel@gmail.com>
2016-04-07 21:15:57 -0400

    Commit

--- start.py
+++ start.py
@@ -4,11 +4,10 @@ update = ['sudo','git','pull']
 bot = ['sudo','python3.5','bot.py']
 up = sub.Popen(update)
 up.wait()
-p = sub.Popen(bot,stdout=sub.PIPE,stderr=sub.PIPE,universal_newlines=True)
+p = sub.Popen(bot,universal_newlines=True)
 while True:
-	if p.returncode == None:
-		out, error = p.communicate()
-		print(out)
-		print(error)
-	elif p.returncode != None:
-		p = sub.Popen(bot,stdout=sub.PIPE,stderr=sub.PIPE)
+	try:
+		p.wait()
+		p = sub.Popen(bot,universal_newlines=True)
+	except KeyboardInterrupt:
+		pass
