6f4965070a03c8608529a742f8403424fa7b3bdc
Funky7Monkey <Funky7Monkey@users.noreply.github.com>
2016-04-12 10:02:23 -0400

    Update start.py

--- start.py
+++ start.py
@@ -1,10 +1,7 @@
 from time import sleep
 import subprocess as sub
-updiscord = ["sudo","pip","install","--upgrade","git+https://github.com/Rapptz/discord.py@async"]
 update = ['sudo','git','pull']
 bot = ['sudo','nohup','python3.5','bot.py']
-up = sub.Popen(updiscord)
-up.wait()
 up = sub.Popen(update)
 up.wait()
 p = sub.Popen(bot,universal_newlines=True)
