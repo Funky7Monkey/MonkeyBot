7bfec4a36bc3b09c1518cb985a80d58bb7c9b671
Funky7Monkey <Funky7Monkey@users.noreply.github.com>
2016-04-14 10:18:17 -0400

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,10 @@
 from time import sleep
 import subprocess as sub
+de = ['sudo','rm','nohup.out']
 update = ['sudo','git','pull']
 bot = ['sudo','nohup','python3.5','bot.py']
+de = sub.Popen(de)
+de.wait()
 up = sub.Popen(update)
 up.wait()
 p = sub.Popen(bot,universal_newlines=True)
