215b7b2af964e24d49390b845aafcc71998a1363
Funky7Monkey <Funky7Monkey@users.noreply.github.com>
2016-04-18 11:43:58 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -98,6 +98,7 @@ async def on_ready():
 	for member in client.get_all_members():
 		if int(member.id) in owner and member not in owners:
 			owners.append(member)
+	global comp
 	comp = ['https://hipiseverything.files.wordpress.com/2013/03/compensation.jpg','https://s-media-cache-ak0.pinimg.com/736x/13/82/03/138203eb61d3ba6faa09a1e44960413d.jpg',
 		'http://img.photobucket.com/albums/v394/xdoctor/l_1ed8c6e94411092f4b3e97f21a8bdb59.jpg','http://s2.quickmeme.com/img/e6/e61ac53e2e3cc669600d1161509d4f5456e33c65a57761ad3d4fc5d5c91c5c21.jpg',
 		'https://updatesfromthefield.files.wordpress.com/2010/10/windowslivewriter496747266b98-5752overcompensation-2.jpg','http://www.toptenz.net/wp-content/uploads/2010/10/big-truck-photo.jpeg']
