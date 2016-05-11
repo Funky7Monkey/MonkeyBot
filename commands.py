369865e55be5f9c8bb186800af1467fcf87492cc
Funky7Monkey <nathanjamesschimmel@gmail.com>
2016-05-11 08:12:11 -0400

    Commit

--- commands.py
+++ commands.py
@@ -38,4 +38,5 @@ async def nickname(client, message, *arg):
 					await client.change_nickname(message.server.get_member(client.user.id), arg)
 async def vote(client, message, *arg):
 	"""Vote on or run polls *Currently does nothing*"""
+	__help__ = "Test inner help"
 	pass
